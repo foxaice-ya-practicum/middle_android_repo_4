5e530ccfbb876228b79746b2faf27b6de991bf43
foxaice-ya-practicum <foxaice.swat@yandex.ru>
2025-11-26 13:19:17 +0700

    task 3

--- app/src/main/java/com/yandex/practicum/middle_homework_4/data/work_manager/WorkManagerServiceImp.kt
+++ app/src/main/java/com/yandex/practicum/middle_homework_4/data/work_manager/WorkManagerServiceImp.kt
@@ -24,12 +24,12 @@ class WorkManagerServiceImp(
     private val settingsRepository: SettingsRepository,
     private val scope: CoroutineScope = CoroutineScope(Job() + Dispatchers.IO)
 ) : WorkManagerService {
-    private var period:Long = DEFAULT_REFRESH_PERIOD
+    private var period: Long = DEFAULT_REFRESH_PERIOD
     private var delayed: Long = FIST_LAUNCH_DELAY
 
     init {
         scope.launch {
-            settingsRepository.state.collect{ setting ->
+            settingsRepository.state.collect { setting ->
                 period = setting.periodic
                 delayed = setting.delayed
                 Log.i(TAG, "DataStoreService get data : period = $period | delayed $delayed")
@@ -45,11 +45,12 @@ class WorkManagerServiceImp(
     }
 
     private fun createRequest(repeat: Long, delayed: Long): PeriodicWorkRequest {
-        val networkConstraints = createConstraints()
-        // Допишите реализацию метода и верните WorkRequest на периодическую задачу для RefreshWorker
-        // Интервал запуска задачи (в минутах)  = repeat.
-        // Отсрочка запуска задачи в (секундах) = delayed.
-        // Не забудьте в билдере указать constraints.
+        return PeriodicWorkRequestBuilder<RefreshWorker>(
+            repeatInterval = repeat, repeatIntervalTimeUnit = TimeUnit.MINUTES
+        )
+            .setInitialDelay(delayed, TimeUnit.SECONDS)
+            .setConstraints(createConstraints())
+            .build()
     }
 
     override fun launchRefreshWork() {
